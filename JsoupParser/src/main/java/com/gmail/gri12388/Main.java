63444c7b661dd1ee639b3f6b64b14975e98bb0ca
Kichigin Grigoriy <gri12388@yandex.ru>
2023-06-19 11:12:27 +0400

    feat: поиск элементов в документе

--- JsoupParser/src/main/java/com/gmail/gri12388/Main.java
+++ JsoupParser/src/main/java/com/gmail/gri12388/Main.java
@@ -1,4 +1,36 @@
 package com.gmail.gri12388;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Iterator;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Document document = Jsoup.connect("https://msk.top-academy.ru/").get();
+            System.out.println(document.title());
+
+            Element body = document.body();
+
+            Elements h1s = body.getElementsByTag("h1");
+            for(Element h1 : h1s) {
+                System.out.println(h1.text());
+            }
+
+//            Elements ps = body.getElementsByTag("p");
+//            for(Element p : ps) {
+//                System.out.println(p.text());
+//            }
+
+            Elements as = body.getElementsByTag("a");
+            Iterator<Element> iterator =  as.iterator();
+            if (iterator.hasNext()) System.out.println(iterator.next().text());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
